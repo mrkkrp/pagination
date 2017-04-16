88247131ae73fe04a8a61781366e2aec568b2a19
mrkkrp <markkarpov@opmbx.org>
2017-04-16 20:15:48 +0300

    Drop the unnecessary inlining

--- Data/Pagination.hs
+++ Data/Pagination.hs
@@ -71,13 +71,11 @@ mkPagination size index
 
 pageSize :: Pagination -> Natural
 pageSize (Pagination size _) = size
-{-# INLINE pageSize #-}
 
 -- | Get page index from a 'Pagination'.
 
 pageIndex :: Pagination -> Natural
 pageIndex (Pagination _ index) = index
-{-# INLINE pageIndex #-}
 
 ----------------------------------------------------------------------------
 -- Paginated data
@@ -134,43 +132,36 @@ paginate (Pagination size index') totalItems f =
 
 paginatedItems :: Paginated a -> [a]
 paginatedItems = pgItems
-{-# INLINE paginatedItems #-}
 
 -- | Get 'Pagination' parameters that were used to create this paginated result.
 
 paginatedPagination :: Paginated a -> Pagination
 paginatedPagination = pgPagination
-{-# INLINE paginatedPagination #-}
 
 -- | Get total number of pages in this collection.
 
 paginatedPagesTotal :: Paginated a -> Natural
 paginatedPagesTotal = pgPagesTotal
-{-# INLINE paginatedPagesTotal #-}
 
 -- | Get total number of items in this collection.
 
 paginatedItemsTotal :: Paginated a -> Natural
 paginatedItemsTotal = pgItemsTotal
-{-# INLINE paginatedItemsTotal #-}
 
 -- | Test whether there are other pages.
 
 hasOtherPages :: Paginated a -> Bool
 hasOtherPages Paginated {..} = pgPagesTotal > 1
-{-# INLINE hasOtherPages #-}
 
 -- | Is there previous page?
 
 hasPrevPage :: Paginated a -> Bool
 hasPrevPage Paginated {..} = pageIndex pgPagination > 1
-{-# INLINE hasPrevPage #-}
 
 -- | Is there next page?
 
 hasNextPage :: Paginated a -> Bool
 hasNextPage Paginated {..} = pageIndex pgPagination < pgPagesTotal
-{-# INLINE hasNextPage #-}
 
 -- | Get range of pages to show before and after current page. This does not
 -- necessarily include the first and the last pages (they are supposed to be
@@ -197,7 +188,6 @@ backwardEllip
   -> Natural           -- ^ Number of pages to show before and after
   -> Bool
 backwardEllip p n = NE.head (pageRange p n) > 2
-{-# INLINE backwardEllip #-}
 
 -- | Forward ellipsis appears when page range (pages around current page to
 -- jump to) has gap between its end and the last page.
@@ -207,7 +197,6 @@ forwardEllip
   -> Natural           -- ^ Number of pages to show before and after
   -> Bool              -- ^ Do we have forward ellipsis?
 forwardEllip p@Paginated {..} n = NE.last (pageRange p n) < pred pgPagesTotal
-{-# INLINE forwardEllip #-}
 
 ----------------------------------------------------------------------------
 -- Exceptions
